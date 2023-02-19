cc7d2976433812e58f09ccb8572d6753b3cb8c87
daciandeveloper <121046550+daciandeveloper@users.noreply.github.com>
2023-02-18 23:34:40 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+"use strict"
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+  //--------------------------Lazyyyyyyyyyyyyyyyyyy----------------------------------------
+  var dropdown = document.getElementsByClassName("dropbtn-over");
+  var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
